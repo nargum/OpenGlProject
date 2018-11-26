050ae7c8533919fee6c8aee76457f37cf512a91e
Kuba <jlobodinsky6@gmail.com>
2018-11-26 15:53:40 +0100

    Assimp linked

---
+++ ZPGProject/ModelLoader.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<assimp/Importer.hpp>// C++ importerinterface
+#include<assimp/scene.h>// aiSceneoutputdata structure
+#include<assimp/postprocess.h>// Post processingflags
+
+class ModelLoader
+{
+public:
+	ModelLoader();
+	~ModelLoader();
+};
+
